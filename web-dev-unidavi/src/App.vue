ca5291983457e15a9adfba1f011e76e9aacce1eb
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-11 20:40:39 -0300

    initial commit

---
+++ web-dev-unidavi/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ProductTable />
+</template>
+
+<script>
+import ProductTable from './components/ProductTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
