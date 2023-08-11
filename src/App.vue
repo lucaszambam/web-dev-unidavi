1954634580fb230713dcf54568fb249715d68e3c
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-11 20:54:16 -0300

    add repo anchor

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <ProductTable />
+  <div class="github-repo">
+    <a href="https://github.com/lucaszambam/web-dev-unidavi" target="_blank">
+      <img src = "./assets/svg/github-mark.svg" alt="GitHub Repository"/>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -16,5 +21,19 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+.github-repo {
+	position: fixed;
+    bottom: 10px;
+    right: 20px;
+}
+.github-repo a img {
+	scale: 0.45;
+    opacity: 0.5;
+	transition: all .25s;
+}
+.github-repo a img:hover {
+	opacity: 1;
+	scale: 0.5;
 }
 </style>
