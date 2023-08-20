b92df8e6492e9bb2b9015e3acc6a8fbc39205be8
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-20 20:52:38 -0300

    ex02 component

---
+++ src/components/Exercise02.vue
@@ -0,0 +1,14 @@
+<template>
+    <strong>Negrito</strong><br>
+    <em>Itálico</em><br>
+    Linha Horizontal<br>
+    <hr>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise02',
+}
+</script>
+
+<style scoped></style>
