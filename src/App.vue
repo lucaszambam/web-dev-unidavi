81ceb9890682ab0ff32ac2c6a689fa45fd2bbbc3
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-20 20:53:06 -0300

    create structure to see the exercises

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
-  <ProductTable />
+  <div>
+    <span>Selecione o exercício:</span>
+    <select name="Selecione o Exercício" @change="onChangeSelect($event)">
+      <option value="0">Exercício Tabela</option>
+      <option value="1">Exercício 01</option>
+      <option value="2">Exercício 02</option>
+      <option value="3">Exercício 03</option>
+    </select>
+  </div>
+
+  <div class="exercise-container">
+    <div :class="{ active: this.exercise == 0 }">
+      <ProductTable />
+    </div>
+    <div :class="{ active: this.exercise == 1 }">
+      <Exercise01 />
+    </div>
+
+    <div :class="{ active: this.exercise == 2 }">
+      <Exercise02 />
+    </div>
+
+    <div :class="{ active: this.exercise == 3 }">
+      <Exercise03 />
+    </div>
+  </div>
+
   <div class="github-repo">
     <a href="https://github.com/lucaszambam/web-dev-unidavi" target="_blank">
-      <img src = "./assets/svg/github-mark.svg" alt="GitHub Repository"/>
+      <img src="./assets/svg/github-mark.svg" alt="GitHub Repository" />
     </a>
   </div>
 </template>
 
 <script>
 import ProductTable from './components/ProductTable.vue'
+import Exercise01 from './components/Exercise01.vue'
+import Exercise02 from './components/Exercise02.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductTable
+    ProductTable,
+    Exercise01,
+    Exercise02
+  },
+  data() {
+    return {
+      exercise: 0,
+    }
+  },
+  methods: {
+    onChangeSelect(event) {
+      this.exercise = event.target.value;
+    }
   }
 }
 </script>
@@ -22,18 +62,25 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+
+.exercise-container > div:not(.active) {
+  display: none;
+}
+
 .github-repo {
-	position: fixed;
-    bottom: 10px;
-    right: 20px;
+  position: fixed;
+  bottom: 10px;
+  right: 20px;
 }
+
 .github-repo a img {
-	scale: 0.45;
-    opacity: 0.5;
-	transition: all .25s;
+  scale: 0.45;
+  opacity: 0.5;
+  transition: all .25s;
 }
+
 .github-repo a img:hover {
-	opacity: 1;
-	scale: 0.5;
+  opacity: 1;
+  scale: 0.5;
 }
 </style>
