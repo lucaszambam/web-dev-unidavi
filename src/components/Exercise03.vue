f95d2f30c6b824efd3e08cdd0e249ae31e3156d4
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-20 21:07:58 -0300

    ex03 component

---
+++ src/components/Exercise03.vue
@@ -0,0 +1,24 @@
+<template>
+    <hr>
+    <p>
+        Lorem ipsum dolor sit amet, <i>consectetur</i> adipiscing elit. Sed <u>vitae</u> purus et diam <b>dictum</b>
+        bibendum. Mauris <span style="color: red;">ullamcorper</span>, <span style="color: blue;">ligula</span> eget
+        interdum <span style="color: green;">congue</span>, <span style="color: purple;">felis</span> enim <span
+            style="color: orange;">consequat</span> elit, in <a href="https://www.example.com" target="_blank">link 1</a>
+        justo urna <a href="https://www.example2.com" target="_blank">link 2</a> eget <a href="https://www.example3.com"
+            target="_blank">link 3</a>. Donec <i>mollis</i> nisi non nisi <u>porta</u>, ac <b>vulputate</b> quam <span
+            style="color: red;">consectetur</span> <span style="color: blue;">adipiscing</span>. Fusce <span
+            style="color: green;">rutrum</span> <span style="color: purple;">aliquam</span> <span
+            style="color: orange;">ante</span> ac <i>lacinia</i>. Nunc non <u>aliquam</u> diam. Nullam <b>quis</b> odio
+        <span style="color: red;">velit</span>.
+    </p>
+    <hr>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise03',
+}
+</script>
+
+<style scoped></style>
