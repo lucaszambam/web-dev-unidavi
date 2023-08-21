1c8ab56d7bdf5590d28ae6067cb557b47d99ca59
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-20 22:47:49 -0300

    adjust to render the components dynamically

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,15 @@
   <div>
     <span>Selecione o exercício:</span>
     <select name="Selecione o Exercício" @change="onChangeSelect($event)">
-      <option value="0">Exercício Tabela</option>
-      <option value="1">Exercício 01</option>
-      <option value="2">Exercício 02</option>
-      <option value="3">Exercício 03</option>
-      <option value="4">Exercício 04</option>
+      <option v-for="index in this.numberOfComponents" :value="index - 1">Exercício 0{{ index - 1 }}</option>
     </select>
   </div>
 
   <div class="exercise-container">
-    <div :class="{ active: this.exercise == 0 }">
-      <ProductTable />
+    <div v-for="index in this.numberOfComponents" :class="{ active: this.exercise == index - 1 }">
+      <component :is="'Exercise0' + (index - 1) "></component>
     </div>
-    <div :class="{ active: this.exercise == 1 }">
-      <Exercise01 />
-    </div>
-
-    <div :class="{ active: this.exercise == 2 }">
-      <Exercise02 />
-    </div>
-
-    <div :class="{ active: this.exercise == 3 }">
-      <Exercise03 />
-    </div>
-
-    <div :class="{ active: this.exercise == 4 }">
-      <Exercise04 />
-    </div>
+    
   </div>
 
   <div class="github-repo">
@@ -39,30 +21,40 @@
 </template>
 
 <script>
-import ProductTable from './components/ProductTable.vue'
+import Exercise00 from './components/Exercise00.vue'
 import Exercise01 from './components/Exercise01.vue'
 import Exercise02 from './components/Exercise02.vue'
 import Exercise03 from './components/Exercise03.vue'
 import Exercise04 from './components/Exercise04.vue'
+import Exercise05 from './components/Exercise05.vue'
+import Exercise06 from './components/Exercise06.vue'
+import Exercise07 from './components/Exercise07.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductTable,
+    Exercise00,
     Exercise01,
     Exercise02,
     Exercise03,
-    Exercise04
+    Exercise04,
+    Exercise05,
+    Exercise06,
+    Exercise07
   },
   data() {
     return {
       exercise: 0,
+      numberOfComponents: 0
     }
   },
   methods: {
     onChangeSelect(event) {
       this.exercise = event.target.value;
     }
+  },
+  beforeMount() {
+    this.numberOfComponents = Object.keys(this.$options.components).length;
   }
 }
 </script>
