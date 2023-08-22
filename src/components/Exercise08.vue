5f7c520589a2de3c040462b74df6fb05227cd289
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-21 23:30:04 -0300

    ex08 component

---
+++ src/components/Exercise08.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br>
+
+        <label>Gênero:</label>
+        <input type="checkbox" id="sexoM" name="sexo" value="M">
+        <label for="sexoM">Masculino</label>
+        <input type="checkbox" id="sexoF" name="sexo" value="F">
+        <label for="sexoF">Feminino</label><br>
+
+        <label for="estadoCivil">Estado Civil:</label>
+        <select id="estadoCivil" name="estadoCivil">
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casado</option>
+            <option value="outros">Outros</option>
+        </select><br>
+
+        <button type="submit">Enviar</button>
+    </form>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise08',
+}
+</script>
+
+<style scoped></style>
