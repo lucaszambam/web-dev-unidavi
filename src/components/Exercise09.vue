e7f7902c6853e945fa5eb3b0f1e99db791f821cb
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-21 23:30:18 -0300

    ex09 component

---
+++ src/components/Exercise09.vue
@@ -0,0 +1,25 @@
+<template>
+    <table border="1">
+        <tr>
+            <td>Usuário:</td>
+            <td><input type="text" id="usuario" name="usuario" required></td>
+        </tr>
+        <tr>
+            <td>Senha:</td>
+            <td><input type="password" id="senha" name="senha" required></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <button type="submit">Enviar</button>
+            </td>
+        </tr>
+    </table>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise09',
+}
+</script>
+
+<style scoped></style>
