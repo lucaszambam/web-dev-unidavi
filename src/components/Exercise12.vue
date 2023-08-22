b00c77431a929ec87723543c77eb3cf41cc5d195
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-21 23:31:02 -0300

    ex12 component

---
+++ src/components/Exercise12.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Minha 1ª Pagina Web - Exercicio 01</h1>
+    <h2>Nome, N° e Turma dos Integrantes</h2>
+    <hr>
+    <p>Colégio Sumgular Informa</p>
+    <p>os alunos do <strong>Curso Técnico em Informatica</strong>, na disciplna de <em>LGP</em>, iniciaram a edição de <u>Paginas Web</u>. Utilizando aprogramacao <em><strong><u>HTML</u></strong></em>.</p>
+    <hr>
+    <p>Destaforma,embreve, eles serao</p>
+    <h3>Web Designers</h3>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise12',
+}
+</script>
+
+<style scoped></style>
