c07a46c6c8d40319179dfae2c5201b0e43bc412e
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-22 23:02:12 -0300

    ex17 component

---
+++ src/components/Exercise17.vue
@@ -0,0 +1,53 @@
+<template>
+    <h1>Listas, Links e Âncoras</h1>
+    <hr>
+    <h2>Nome dos Integrantes</h2>
+    <hr>
+    <ol>
+        <li><u>Nome Completo Aluno 1</u></li>
+        <li><u>Nome Completo Aluno 2</u></li>
+        <li><u>Nome Completo Aluno 3</u></li>
+    </ol>
+    <hr>
+    <h2>Dados Pessoais</h2>
+    <hr>
+    <ol>
+        <li>Apelido do ALuno 1
+            <ul>
+                <li>Endereço</li>
+                <li>Data de Nascimento</li>
+                <li>Passatempos</li>
+                <li>Enviar <a href="mailto:lucas.zambam@unidavi.edu.br">E-Mail</a></li>
+                <li></li>
+            </ul>
+        </li>
+
+        <li>Apelido do ALuno 2
+            <ul>
+                <li>Endereço</li>
+                <li>Data de Nascimento</li>
+                <li>Passatempos</li>
+                <li>Enviar <a href="mailto:lucas.zambam@unidavi.edu.br">E-Mail</a></li>
+                <li></li>
+            </ul>
+        </li>
+
+        <li>Apelido do ALuno 3
+            <ul>
+                <li>Endereço</li>
+                <li>Data de Nascimento</li>
+                <li>Passatempos</li>
+                <li>Enviar <a href="mailto:lucas.zambam@unidavi.edu.br">E-Mail</a></li>
+                <li></li>
+            </ul>
+        </li>
+    </ol>
+</template>
+  
+<script>
+export default {
+    name: 'Exercise06',
+}
+</script>
+
+<style scoped></style>
