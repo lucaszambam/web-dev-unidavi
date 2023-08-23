ea16b2c6e70845fc517c6ed0823e4e858705d6d6
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-22 23:01:31 -0300

    ex13 component

---
+++ src/components/Exercise13.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>Minha 2ª Pagina Web - Exercicio 02</h1>
+    <h2>Nome, N° e Turma dos Integrantes</h2>
+    <hr>
+    <p>Listas Odenadas</p>
+    <ol>
+        <li>Volkswagen
+            <ol>
+                <li>Gol</li>
+                <li>Parati</li>
+                <li>Golf</li>
+            </ol>
+        </li>
+        <li>Fiat
+            <ol>
+                <li>Uno</li>
+                <li>Palio</li>
+                <li>Stilo</li>
+            </ol>
+        </li>
+    </ol>
+    <hr>
+    <p>Listas Não Odenadas</p>
+    <ul>
+        <li>General Motors
+            <ul>
+                <li>Celta</li>
+                <li>Vectra</li>
+                <li>Astra</li>
+            </ul>
+        </li>
+        <li>Ford
+            <ul>
+                <li>Ka</li>
+                <li>Fiesta</li>
+                <li>Ecosport</li>
+            </ul>
+        </li>
+    </ul>
+    
+</template>
+  
+<script>
+export default {
+    name: 'Exercise13',
+}
+</script>
+
+<style scoped></style>
