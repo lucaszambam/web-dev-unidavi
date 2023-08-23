3ad7c47ca806da4396061075c6cb338ad052608f
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-22 23:01:51 -0300

    ex15 component

---
+++ src/components/Exercise15.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img src="https://media.tenor.com/VOdWjm2zbEAAAAAC/gandalf-sax-guy.gif" alt="Gandalf">
+        <span>Imagem GIF</span>
+    </div>
+        
+    <div>
+        <img src="https://www.metroworldnews.com.br/resizer/SeUno3_rJMF1-yzZnSwFQxF-m_4=/1440x1080/filters:format(png):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/J55AOQTE2FCANNSHH4U4PJX3F4.png" alt="Gandalf">
+        <span>Imagem JPG</span>
+    </div>
+
+    <div>
+        <img src="https://www.alarichall.org.uk/lotr/gandalf1.bmp" alt="gandalf">
+        <span>Imagem BMP</span>
+    </div>
+
+    <a href="https://github.com/lucaszambam">
+        <img style="width:100px;" src="https://3.bp.blogspot.com/-ImvdVkA6sd4/TnFJ_aggZJI/AAAAAAAAA8A/QauQ23L3VOs/w1200-h630-p-k-no-nu/%255BFig.+24%255D+Gandalf+luta+com+Balrog.bmp" alt="">
+        <span>Link, comprimento 100px</span>
+    </a><br>
+
+    <a href="https://github.com/lucaszambam">
+        <img style="height:50px;" src="https://3.bp.blogspot.com/-ImvdVkA6sd4/TnFJ_aggZJI/AAAAAAAAA8A/QauQ23L3VOs/w1200-h630-p-k-no-nu/%255BFig.+24%255D+Gandalf+luta+com+Balrog.bmp" alt="">
+        <span>Link, altura 50px</span>
+    </a>
+    
+</template>
+  
+<script>
+export default {
+    name: 'Exercise15',
+}
+</script>
+
+<style scoped></style>
