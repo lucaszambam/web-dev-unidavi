bd7260dd6deddbc677a1f3cc82d3e157172d4075
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-22 23:01:43 -0300

    ex14 component

---
+++ src/components/Exercise14.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>Minha 3ª Pagina Web - Exercicio 03</h1>
+    <h2>Nome, N° e Turma dos Integrantes</h2>
+    <hr>
+    <p>Listas Aninhadas Mistas - I</p>
+    <ol>
+        <li>Aluno 1
+            <ul>
+                <li>site</li>
+                <li>telefone</li>
+            </ul>
+        </li>
+        <li>Aluno 2
+            <ul>
+                <li>site</li>
+                <li>telefone</li>
+            </ul>
+        </li>
+    </ol>
+    <hr>
+    <p>Listas Aninhadas Mistas - II</p>
+    <ul>
+        <li>Disciplineas do 2º ano de informática
+            <ol>
+                <li>matéria 1</li>
+                <li>matéria 2</li>
+                <li>matéria 3</li>
+            </ol>
+        </li>
+        <li>Disciplineas do 2º ano de informática
+            <ol>
+                <li>matéria 1</li>
+                <li>matéria 2</li>
+                <li>matéria 3</li>
+            </ol>
+        </li>
+    </ul>
+    
+</template>
+  
+<script>
+export default {
+    name: 'Exercise14',
+}
+</script>
+
+<style scoped></style>
