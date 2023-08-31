3509a137a6ec23b483dc28a4ecc660a9a37bf4f4
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-31 20:01:05 -0300

    add ex20 component

--- src/components/Exercise20.vue
+++ src/components/Exercise20.vue
@@ -1,8 +1,137 @@
 <template>
-    <div >
+    <div>
 
+        <body>
+            <header>
+                <h1>Logo</h1>
+
+                <nav>
+                    <h3>Menu</h3>
+                    <h3>Menu</h3>
+                    <h3>Menu</h3>
+                    <h3>Menu</h3>
+                    <h3>Menu</h3>
+                </nav>
+
+            </header>
+
+            <section>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+                <div class="photo-container">
+                    <div class="photo">
+                        <span>Foto</span>
+                    </div>
+                    <div>
+                        <p>à 37 minutos</p>
+                        <h4>Título</h4>
+                    </div>
+                </div>
+            </section>
+
+            <div class="bottom-button">
+                <h3>Mais</h3>
+            </div>
+
+        </body>
     </div>
-    
 </template>
   
 <script>
@@ -12,4 +141,72 @@ export default {
 </script>
 
 <style scoped>
+body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+section {
+    width: 80vw;
+    height: 100vh;
+    display: flex;
+    margin-top: 2%;
+    gap: 20px;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+header h1 {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    font-size: 42pt;
+}
+
+nav {
+    display: flex;
+    gap: 125px;
+    justify-content: center;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
+    width: 100vw;
+}
+
+.photo-container {
+    display: flex;
+    flex-direction: column;
+}
+
+.photo {
+    height: 225px;
+    width: 225px;
+    background: rgb(180, 180, 180);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.bottom-button {
+    width: 450px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(180, 180, 180);
+    margin-top: 5%;
+}
+
+.bottom-button h3 {
+
+    font-weight: lighter;
+}
 </style>
